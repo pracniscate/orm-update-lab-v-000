27793c5c81df9239c4ed72b3d54cf286aecf040b
Lorena <lorena.giddens@outlook.com>
2018-07-20 14:16:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,4 +50,10 @@ class Student
     DB[:conn].execute(sql, self.name, self.grade, self.name)
   end
 
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save 
+    student
+  end
+
 end
