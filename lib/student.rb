95736ce8294996f64984fab1f7e8645d73f46fca
Lorena <lorena.giddens@outlook.com>
2018-07-20 14:35:36 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -27,4 +27,47 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def save
+    # update a record if called on an object that is already persisted
+    if self.id
+      self.update
+    else
+      # insert a new row into the database
+      sql = <<-SQL
+        INSERT INTO students (name, grade)
+        VALUES (?, ?)
+      SQL
+
+      DB[:conn].execute(sql, self.name, self.grade)
+
+      # assign the id attribute
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
+
+  def self.new_from_db(row)
+    # convert database data into a Ruby object
+    new_student = self.new(row[0], row[1], row[2])
+    new_student
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+
 end
