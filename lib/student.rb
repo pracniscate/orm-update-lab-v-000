c70026dc612bcff8f68211a876c4fe04fff2116d
Lorena <lorena.giddens@outlook.com>
2018-07-20 14:30:04 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,10 +52,7 @@ class Student
 
   def self.new_from_db(row)
     # convert database data into a Ruby object
-    new_student = self.new
-    new_student.id = row[0]
-    new_student.name = row[1]
-    new_student.length row[2]
+    new_student = self.new(row[0], row[1], row[2])
     new_student
   end
 
